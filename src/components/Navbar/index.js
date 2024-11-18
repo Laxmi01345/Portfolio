1f938336f89a31116a901401e34c69115de9fd91
laxmi013 <laxmiray013@gmail.com>
2024-11-18 12:44:19 +0530

    final commit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Nav, NavLink, NavbarContainer,NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
+import { Nav, NavLink, NavbarContainer,NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavbarStyledComponent'
 
 import { FaBars } from 'react-icons/fa';
 import { Bio } from '../../data/constants';
